889bf461c12c4db376ef8117c4e6c3b44f6f2f6c
Paul Brunner <trite@users.noreply.github.com>
2023-07-08 01:13:31 -0700

    Convert from querying a single entry to multiple

--- src/components/Newsfeed.tsx
+++ src/components/Newsfeed.tsx
@@ -7,7 +7,8 @@ import type { NewsfeedQuery as NewsfeedQueryType } from "./__generated__/Newsfee
 
 const NewsfeedQuery = graphql`
   query NewsfeedQuery {
-    topStory {
+    topStories {
+      id
       ...StoryFragment
     }
   }
@@ -16,11 +17,13 @@ const NewsfeedQuery = graphql`
 export default function Newsfeed({}) {
   const data = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
 
-  const story = data.topStory;
+  const stories = data.topStories;
 
   return (
     <div className="newsfeed">
-      <Story story={story} />
+      {stories.map((story) => (
+        <Story key={story.id} story={story} />
+      ))}
     </div>
   );
 }
